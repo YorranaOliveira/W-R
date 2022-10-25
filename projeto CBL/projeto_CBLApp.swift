2ff83cc40e11bd1442779d3c9852d154a8896a7a
gomeez <106769350+amordanadja@users.noreply.github.com>
2022-10-25 17:07:26 -0300

    Initial Commit

---
+++ projeto CBL/projeto_CBLApp.swift
@@ -0,0 +1,17 @@
+//
+//  projeto_CBLApp.swift
+//  projeto CBL
+//
+//  Created by Ana Beatriz Costa Gomes on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct projeto_CBLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
